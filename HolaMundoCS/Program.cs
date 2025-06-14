04c9f6c76aec7be8c654d64f6d0ddb968c70ff5d
rober <rober@VALOY>
2025-06-13 23:24:12 -0400

    Agregar archivos de proyecto.

---
+++ HolaMundoCS/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HolaMundoCS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola Mundo desde C#!");
+        }
+    }
+}
